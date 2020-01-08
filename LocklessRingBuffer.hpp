c5991c4ac1c8a077e662488b22767cb7419deb2e
lambda <lambda.g.luo>
2020-01-08 11:57:59 +0800

    good

--- LocklessRingBuffer.hpp
+++ LocklessRingBuffer.hpp
@@ -9,15 +9,19 @@ private:
     int size;
     int capacity;
 
-
-    void resize(int cap)
+    int _IsEmpty()
     {
-        
+        return read_ptr == write_ptr;
+    }
+
+    int _IsFull()
+    {
+        return (write_ptr+1)%capacity == read_ptr;
     }
 
 public:
     LocklessRingBuffer(int cap):
-        capacity(cap),
+        capacity(cap+1),
         read_ptr(0),
         write_ptr(0),
         size(0)
@@ -30,14 +34,42 @@ public:
         delete[] buf;
     }
 
-    int read(int len, T* data)
+    int Read(int len, T* data)
     {
 
     }
 
-    int write(int len, T* data)
+    int Write(int len, T* data)
     {
 
     }
 
+    void Resize(int cap)
+    {
+        
+    }
+
+    int GetFree()
+    {
+        if(_IsEmpty())
+            return capacity;
+        else if(_IsFull())
+            return 0;
+        else
+        {
+            if(write_ptr > read_ptr)
+            {
+                return (capacity - write_ptr) + read_ptr;
+            }
+            else
+            {
+                return read_ptr - write_ptr;
+            }
+        }
+    }
+
+    int GetSize()
+    {
+        return capacity - GetFree();
+    }
 };
